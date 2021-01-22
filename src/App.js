8b2e959a617941a5f5f6bf49b1c89bd63ec061f5
Iryna Arkhypchenko <37odri@gmail.com>
2021-01-22 17:10:51 +0200

    [4]:  jsx

--- src/App.js
+++ src/App.js
@@ -3,21 +3,13 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", { label: "hello" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Dog",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
-  ]);
+  return (
+    <div>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  );
 };
 
-// Эта функция стирает все, что внутри div#rooot
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
