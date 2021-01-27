94637b0d59afe9fd9d1e5217fbb0c68b6e4d9858
Iryna Arkhypchenko <37odri@gmail.com>
2021-01-27 21:38:19 +0200

    [6]: effects

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -6,14 +6,17 @@ const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
 
+  // useEffect is disconnected from when the render is happening. useEffect is scheduling this function to run after the render happens. So: first render -> useEffect function (but not immidiately, after some time)
   useEffect(() => {
-    pet.breeds(animal).then;
-    return () => {
-      cleanup;
-    };
-  }, [input]);
+    setBreeds([]);
+    setBreed("");
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error); // same as (err) => console.error(err)
+  }, [animal, setBreeds, setBreed]);
 
   const handleLocationChange = ({ target }) => setLocation(target.value);
   return (
