96f0e5dcbff04281583349b0ac85ae66197aaaee
Iryna Arkhypchenko <37odri@gmail.com>
2021-01-28 14:53:34 +0200

    [12]: Portals

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import pet from "@frontendmasters/pet";
+import { navigate } from "@reach/router";
 import Carousel from "./Carousel";
 import ErrorBoundry from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
+import Modal from "./Modal";
 
 class Details extends Component {
   // вот этот синтаксис очень неудобный. Чтобы писать меньше кода - нужно настроить babel
@@ -15,7 +17,7 @@ class Details extends Component {
   // }
 
   // with babel it'll look like this
-  state = { loading: true, name: "" };
+  state = { loading: true, name: "", showModal: false };
 
   componentDidMount() {
     // throw new Error("Check error Boundry component");
@@ -30,6 +32,7 @@ class Details extends Component {
         this.setState({
           // shalow merge: Object.assign(oldState, newState). If I have nested objects - they will NOT be overwritten so setState does only top level
           name: animal.name,
+          url: animal.url,
           animal: animal.type,
           location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
           description: animal.description,
@@ -41,11 +44,23 @@ class Details extends Component {
     );
   }
 
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+
+  adopt = () => navigate(this.state.url);
+
   render() {
     if (this.state.loading) {
       return <h1>loading … </h1>;
     }
-    const { animal, breed, location, description, name, media } = this.state;
+    const {
+      animal,
+      breed,
+      location,
+      description,
+      name,
+      media,
+      showModal,
+    } = this.state;
     return (
       <div className="details">
         <Carousel media={media} />
@@ -55,10 +70,26 @@ class Details extends Component {
           {/* Using Context with Class Components */}
           <ThemeContext.Consumer>
             {([theme]) => (
-              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+              <button
+                style={{ backgroundColor: theme }}
+                onClick={this.toggleModal}
+              >
+                Adopt {name}
+              </button>
             )}
           </ThemeContext.Consumer>
           <p>{description}</p>
+          {showModal && (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}</h1>
+                <div className="buttons">
+                  <button onClick={this.adopt}>Yes</button>
+                  <button onClick={this.toggleModal}>No</button>
+                </div>
+              </div>
+            </Modal>
+          )}
         </div>
       </div>
     );
