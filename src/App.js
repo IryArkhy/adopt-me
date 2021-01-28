9562dce4795dfab79b6d9c8601f0972bdc29763f
Iryna Arkhypchenko <37odri@gmail.com>
2021-01-28 12:17:39 +0200

    [8]: Async & Routing
    - async api requests
    - mock environment to work offline
    - reach router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { render } from "react-dom";
+import { Link, Router } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
-        <h1>Adopt me!</h1>
-        <SearchParams />
+        <header>
+          <Link to="/">Adopt me!</Link>
+        </header>
+        <Router>
+          {/* Reach router has a scoring algorithm so it does not matter in which order you place your components */}
+          {/* React router uses a Switch component and will render the first match so the order in React router IS important */}
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,21 @@
 import React, { useState, useEffect } from "react";
 import useDropdown from "./useDropdown";
 import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  // async functions return a Promise
+  async function requestPets() {
+    const { animals } = await pet.animals({ location, breed, type: animal });
+
+    setPets(animals || []);
+  }
 
   // useEffect is disconnected from when the render is happening. useEffect is scheduling this function to run after the render happens. So: first render -> useEffect function (but not immidiately, after some time)
   useEffect(() => {
@@ -18,10 +27,15 @@ const SearchParams = () => {
     }, console.error); // same as (err) => console.error(err)
   }, [animal, setBreeds, setBreed]);
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    requestPets();
+  }
+
   const handleLocationChange = ({ target }) => setLocation(target.value);
   return (
     <div className="search-params">
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="location">
           Loaction
           <input
@@ -35,6 +49,7 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
