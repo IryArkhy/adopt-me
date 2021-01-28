8598fe3882ad7804acb44a62528037f7f2610643
Iryna Arkhypchenko <37odri@gmail.com>
2021-01-28 13:25:58 +0200

    [9]: class components

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
+import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
-const Details = (props) => {
-  return (
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
-  );
-};
+class Details extends Component {
+  // вот этот синтаксис очень неудобный. Чтобы писать меньше кода - нужно настроить babel
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     loading: true,
+  //   };
+  // }
+
+  // with babel it'll look like this
+  state = { loading: true, name: "" };
+
+  componentDidMount() {
+    // runs only once when I first get created
+    // AJAX request
+
+    //!!! it's required to use an arrow function here
+    // if I use a regular function it'll create a new context and this != Details Component
+    // arrow functions do not create a new context
+    pet.animal(this.props.id).then(
+      ({ animal }) =>
+        this.setState({
+          // shalow merge: Object.assign(oldState, newState). If I have nested objects - they will NOT be overwritten so setState does only top level
+          name: animal.name,
+          animal: animal.type,
+          location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+          description: animal.description,
+          media: animal.photos,
+          breed: animal.breeds.primary,
+          loading: false,
+        }),
+      console.error
+    );
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h1>loading … </h1>;
+    }
+    const { animal, breed, location, description, name, media } = this.state;
+    return (
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
